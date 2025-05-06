9a351c63bc54b3f673c4e9cf58e8860dbfbe0ede
{MuhammadAhhadQumar} <{ahhadqamar1@gmail.com}>
2025-05-06 19:52:40 +0500

    enhance App component with fade-in effect and style improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 export default function App() {
-  document.addEventListener('load', () => {})
+  document.addEventListener('DOMContentLoaded', () => {
+    const appElement = document.querySelector('h1')
+    appElement.style.opacity = 0
+    appElement.style.transition = 'opacity 1s ease-in-out'
+
+    setTimeout(() => {
+      appElement.style.opacity = 1
+    }, 100)
+  })
 
   return (
     <div>
-      <h1>Welcome to Filmpire</h1>
+      <h1 className="bg-red-400 py-4 text-center font-bold leading-5 text-2xl text-white shadow-md shadow-red-500/50">
+        Welcome to Filmpire
+      </h1>
     </div>
   )
 }
